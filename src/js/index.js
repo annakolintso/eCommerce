a62d2f3f240a2ef16568de593bf7aa6b74d220b0
anna.kolintso <anna.kolintso@gmail.com>
2020-02-04 18:59:05 +0200

    add product-view page

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,21 @@
 "use strict";
-// let favouriteFilm = {};
-// favouriteFilm.name = "Maleficent";
-// favouriteFilm.year = 2019;
-// favouriteFilm.country = "USA";
-// favouriteFilm.actor = {
-//     name: "Angelina",
-//     surname: "Joly"
-// };
-// favouriteFilm.timeLine = "119 min";
-// favouriteFilm.genre = "fantasy";
-// favouriteFilm.isWorthWatch = true;
-// let question = confirm(`Have you watched ${favouriteFilm.name}?`);
-// if (question) {
-//     favouriteFilm.isWorthWatch = true;
-// } else {
-//     favouriteFilm.isWorthWatch = false;
-// }
-//
-//
-// alert("It worth watching");
-
 
 $(document).ready(function() {
-    $('select').select2({
+    $('#currencySelect').select2({
         minimumResultsForSearch: -1,
+        dropdownCssClass: 'currency-dropdown',
+        width: '53px'
+    });
+    $('#colorSelect').select2({
+        minimumResultsForSearch: -1,
+        dropdownCssClass: 'settings-dropdown',
+        placeholder: "Select color",
+        width: '170px'
+    });
+    $('#sizeSelect').select2({
+        minimumResultsForSearch: -1,
+        dropdownCssClass: 'settings-dropdown',
+        placeholder: "Select size",
+        width: '170px'
     });
 });
---
+++ src/js/slick-carousel.js
@@ -0,0 +1,6 @@
+
+$('.product__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
